9234d555b480bd0fea080da239a514494d4a0384
Artem Dunaev <dua@crtweb.ru>
2019-03-07 16:51:40 +0500

    Updates

---
+++ precache-manifest.e44104fa4262f8c5397eedb2d7ea6218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ceef7fcbd35532b1b5",
+    "url": "/react/static/js/runtime~main.b2ceef7f.js"
+  },
+  {
+    "revision": "79000ebd515b6fbb4d5f",
+    "url": "/react/static/js/main.79000ebd.chunk.js"
+  },
+  {
+    "revision": "9fa29b4c50f61aeddd08",
+    "url": "/react/static/js/2.9fa29b4c.chunk.js"
+  },
+  {
+    "revision": "79000ebd515b6fbb4d5f",
+    "url": "/react/static/css/main.ab669f62.chunk.css"
+  },
+  {
+    "revision": "3b91f1b778c54a5dd26a96da6888eb22",
+    "url": "/react/index.html"
+  }
+];
