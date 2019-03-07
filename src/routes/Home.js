21523f9ef1f6a41f0377d5cb5477fbb80c7831ef
Artem Dunaev <dua@crtweb.ru>
2019-03-07 14:38:33 +0500

    change jsx to js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => (
   <div>
     <h1>Welcome to test React App with React Router v4</h1>
+    <p>Select a page with component you want to visit</p>
   </div>
 )
 
